5f2d04f84d838834e6e4db825ebdc0f16b06d5e0
Vpazinatto <40928534+Vpazinatto@users.noreply.github.com>
2018-11-09 16:23:41 -0200

    src

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,12 @@
+class NegociacaoController {
+    adiciona(event) {
+        event.preventDefault();
+
+        let $ = document.querySelector;
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+
+    }
+}
