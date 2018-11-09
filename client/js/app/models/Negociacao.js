585b929ae5b1d8b8874a80c44c86bb6dd7ed38a8
Vpazinatto <40928534+Vpazinatto@users.noreply.github.com>
2018-11-09 14:44:54 -0200

    src

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,25 @@
+class Negociacao {
+
+    constructor(data, qtd, valor) {
+        this._data = data;
+        this._quantidade = qtd;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+}
